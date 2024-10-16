13b880cd12e9fffa627a9b3db275839301602365
Naim Sulejmani <naim.sulejmani@gmail.com>
2024-10-16 12:40:33 +0200

    ADDED htmls

--- src/main/java/dev/naimsulejmani/webdevelopmentgrupi1/controllers/ExampleController.java
+++ src/main/java/dev/naimsulejmani/webdevelopmentgrupi1/controllers/ExampleController.java
@@ -1,4 +1,20 @@
 package dev.naimsulejmani.webdevelopmentgrupi1.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ExampleController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
 }
